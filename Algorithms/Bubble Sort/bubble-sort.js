9a6ed1715695ae5d0ee5787ae168e4300624c4c8
matweaver7 <matweaver7@gmail.com>
2021-05-26 22:36:59 -0600

    added bubble sort and test cases

--- Algorithms/Bubble Sort/bubble-sort.js
+++ Algorithms/Bubble Sort/bubble-sort.js
@@ -6,15 +6,22 @@
 //          Add a variable the 
 // Given [4,5,66,2,43] sort in ascending to produce [2,4,5,43,66]
 
+const swap = (pos1, pos2, list) => {
+    let tmp = list[pos1];
+    list[pos1] = list[pos2];
+    list[pos2] = tmp; 
+}
+
 const bubbleSort = (list) => {
     for (let i = 0; i < list.length; i++) {
         for (let t = 0; t < list.length; t++) {
             if ((t+1) < list.length && list[t] > list[t+1]) {
-                swap(list[t], list[t+1]);
+                swap(t, t+1, list);
             }
         
         }
     }
+    return list;
 }
 
 export default bubbleSort;
