088a8605df11dc00ec7522e934b98c15d61bb361
matweaver7 <matweaver7@gmail.com>
2021-05-26 23:42:54 -0600

    added insertion sort

---
+++ Algorithms/Insertion Sort/insertion-sort.js
@@ -0,0 +1,28 @@
+// the whole idea of bubble sort is to filter items going one by one
+// O(n^2)
+// it creates a small mini list (of two) and then addes sorting through the list
+// Step 1 − If it is the first element, it is already sorted. return 1;
+// Step 2 − Pick next element
+// Step 3 − Compare with all elements in the sorted sub-list
+// Step 4 − Shift all the elements in the sorted sub-list that is greater than the 
+//          value to be sorted
+// Step 5 − Insert the value
+// Step 6 − Repeat until list is sorted
+// on small datasets (10-20) it's one of the fastest sorts
+
+
+const insertionSort = (list) => {
+    if (list.length <= 1) return list;
+    for (let i = 1; i < list.length; i++) {
+        let current = list[i];
+        let j = i-1; 
+        while ((j > -1) && (current < list[j])) {
+            list[j+1] = list[j];
+            j--;
+        }
+        list[j+1] = current;
+    }
+    return list;
+}
+
+export default insertionSort;
---
+++ Algorithms/Insertion Sort/insertion-sort.test.js
@@ -0,0 +1,47 @@
+import InsertionSort from "./insertion-sort"
+
+describe('Insertion Sort Tests', () => {
+    const list = [
+        {
+            input:[22, 44, 67, 78, 9],
+            expected: [9,22,44,67,78],
+            description: "A Normal low at end"
+        },
+        {
+            input:[9, 22, 44, 67, 78],
+            expected: [9,22,44,67,78],
+            description: "Normal Flow low at beginning"
+        },
+        {
+            input:[3, 25, 16, 44, 92, 63, 78],
+            expected: [3, 16, 25, 44, 63, 78, 92],
+            description: "Normal flow with mixed"
+        },
+        {
+            input:[3, 16, 25, 25, 44, 92, 63, 78],
+            expected: [3, 16, 25, 25, 44, 63, 78, 92],
+            description: "Normal flow with mixed duplicates"
+        },
+        {
+            input:[1,1],
+            expected: [1,1],
+            description: "Mixed duplicates"
+        },
+        {
+            input:[1],
+            expected: [1],
+            description: "Array with one item"
+        },
+        {
+            input:[],
+            expected: [],
+            description: "Empty Array"
+        },
+        
+    ];
+    list.forEach((testcase) => {
+        test(testcase.description, () => {
+            expect(InsertionSort(testcase.input)).toStrictEqual(testcase.expected);
+        });
+    });
+});
